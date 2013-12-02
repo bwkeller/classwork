4c8534a6125cc1468307947503ad36a8959b9c8d
Ben Keller <malzraa@gmail.com>
2013-12-02 17:12:56 -0500

    wits end, rewriting everything

--- phys719/HW2/godunov.c
+++ phys719/HW2/godunov.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+#include <assert.h>
 #include <math.h>
 #define EPS 1e-6 //Tolerance for iterative scheme
 #define GAMMA 1.4 //Adiabatic index 7/5 for diatomic fluid
 #define a(s) sqrt(GAMMA*s.p/s.rho) //Macro for calculating the soundspeed a
 
-#define TIME 1 //Total time to run for
+#define TIME  1//Total time to run for
 #define NCELLS 400 //Size of the domain in cells
 #define XMAX 2 //Physical size of domain
 #define DX 2.0*XMAX/NCELLS //Grid spacing
-/*#define DT DX/2 //Timestep size*/
-#define DT 2
+#define DT DX/5 //Timestep size
 
 //This struct defines the u state vector that the Fast Riemann Solver wants
 typedef struct state
@@ -27,6 +27,22 @@ typedef struct flux
 	float energydot;
 } flux;
 
+//Get a conservative flux from the FRS state variables
+flux fluxify(state in)
+{
+    flux out;
+    out.rhodot = in.v*in.rho;
+    out.momentumdot = in.rho*in.v*in.v+in.p;
+    out.energydot = in.v*(0.5*in.rho*in.v*in.v+in.p*(1+1.0/(GAMMA-1)));
+    return out;
+}
+
+//Get an intermediate state from a rarefaction fan
+state intermediate(float vhead, float vtail, float p, float rho)
+{
+    state out;
+    out.v = vtail;
+}
 int read_input(state domain[], char filename[])
 {
 	/* Input file format should store the ICs in the format:
@@ -74,7 +90,7 @@ float guess_velocity(state left, state right)
 	return (vtl*z+vtr)/(1+z);
 }
 
-flux iterate(state left, state right)
+flux iterate(state left, state right, int j)
 {
 	flux output = {0,0,0};
 	float vstar = guess_velocity(left, right);
@@ -114,14 +130,114 @@ flux iterate(state left, state right)
 		}
 		vstar -= (pl-pr)/(plp-prp);
 	}
+    if (vstar >= 0)//Right-moving contact
+    {
+        if (vstar <= left.v)//use left shock
+        {
+            if(left.v+a(left)*Wl > 0)//left shock is right moving, use left state
+            {
+                /*assert(0);*/
+                return fluxify(left);
+            }
+            else//left shock is left moving, use post-shock state
+            {
+                al = a(left)*sqrt(((GAMMA+1)+(GAMMA-1)*pl/left.p)/((GAMMA+1)+(GAMMA-1)*left.p/pl));
+                state post = {left.v+a(left)*Wl, pl, GAMMA*pl/pow(al,2)};
+                return fluxify(post);
+            }
+        }
+        else//Use left-rarefaction
+        {
+            if(left.v-a(left) > 0)//Head is right moving, use left state
+            {
+                /*assert(0);*/
+                return fluxify(left);
+            }
+            //Great, now we need to get a post-fan velocity...
+            float vpost;
+            if(vstar >= right.v)//We've got a right shock
+            {
+                al = a(left)*sqrt(((GAMMA+1)+(GAMMA-1)*pl/left.p)/((GAMMA+1)+(GAMMA-1)*left.p/pl));
+                vpost =right.v+a(right)*Wr;
+            }
+            else
+            {
+                vpost = vstar;
+            }
+            if(vstar-al < 0)//Tail is left-moving, use post-fan state
+            {
+                state post = {vpost, pl, GAMMA*pl/pow(al,2)};
+                return fluxify(post);
+            }
+            else//Head is left-moving, tail is right-moving: use interpolated state
+            {
+                float vdot = (vpost-left.v)/(vstar-al-left.v+a(left));
+                float pdot = (pl-left.p)/(vstar-al-left.v+a(left));
+                float rhodot = (GAMMA*pl/pow(al,2)-left.rho)/(vstar-al-left.v+a(left));
+                state interp = {vpost-vdot*(vstar-al), pl-pdot*(vstar-al), GAMMA*pl/pow(al,2)-rhodot*(vstar-al)};
+                /*assert(0);*/
+                return fluxify(interp);
+            }
+        }
+    }
+    else//Left-moving contact
+    {
+        if (vstar >= right.v)//use right shock
+        {
+            if(right.v+a(right)*Wr < 0)//right shock is left moving, use right state
+            {
+                /*assert(0);*/
+                return fluxify(right);
 
-	return output;
+            }
+            else//right shock is right moving, use post-shock state
+            {
+                ar = a(right)*sqrt(((GAMMA+1)+(GAMMA-1)*pr/right.p)/((GAMMA+1)+(GAMMA-1)*right.p/pr));
+                state post = {right.v+a(right)*Wr, pr, GAMMA*pr/pow(ar,2)};
+                return fluxify(post);
+            }
+        }
+        else//Use right-rarefaction
+        {
+            if(right.v+a(right) < 0)//Head is left moving, use right state
+            {
+                /*assert(0);*/
+                return fluxify(right);
+            }
+            //Great, now we need to get a post-fan velocity...
+            float vpost;
+            if(vstar <= left.v)//We've got a left shock
+            {
+                ar = a(right)*sqrt(((GAMMA+1)+(GAMMA-1)*pr/right.p)/((GAMMA+1)+(GAMMA-1)*right.p/pr));
+                vpost =left.v+a(left)*Wl;
+            }
+            else
+            {
+                vpost = vstar;
+            }
+            if(vstar+ar > 0)//Tail is right-moving, use post-fan state
+            {
+                state post = {vpost, pr, GAMMA*pr/pow(ar,2)};
+                return fluxify(post);
+            }
+            else//Head is right-moving, tail is left-moving: use interpolated state
+            {
+                float vdot = (vpost-right.v)/(vstar+ar-right.v-a(right));
+                float pdot = (pr-right.p)/(vstar+ar-right.v-a(right));
+                float rhodot = (GAMMA*pr/pow(ar,2)-right.rho)/(vstar+ar-right.v-a(right));
+                state interp = {vpost-vdot*(vstar+ar), pr-pdot*(vstar+ar), GAMMA*pr/pow(ar,2)-rhodot*(vstar+ar)};
+                /*assert(0);*/
+                return fluxify(interp);
+            }
+        }
+    }
+
 }
 
 void update(state domain[], flux fluxes[])
 {
 	int i;
-	for(i=0; i<NCELLS; i++)
+	for(i=1; i<NCELLS-1; i++)
 	{
 		//convert state variables into their conserved versions
 		float rho = domain[i].rho;
@@ -150,21 +266,20 @@ void evolve(state domain[])
 		printf("Integrating, %f complete\n", (float)i/(float)TIME);
 		for(j=0; j<NCELLS; j++)
 		{
-			/*printf("position %d ", j);*/
 			if (j == 0)//Lefthand boundary
 			{
-				left = iterate(domain[j], domain[j]);
-				right = iterate(domain[j], domain[j+1]);
+				left = iterate(domain[j], domain[j], j);
+				right = iterate(domain[j], domain[j+1], j);
 			}
 			else if(j == NCELLS-1)//Righthand boundary
 			{
-				left = iterate(domain[j-1], domain[j]);
-				right = iterate(domain[j], domain[j]);
+				left = iterate(domain[j-1], domain[j], j);
+				right = iterate(domain[j], domain[j], j);
 			}
 			else
 			{
-				left = iterate(domain[j-1], domain[j]);
-				right = iterate(domain[j], domain[j+1]);
+				left = iterate(domain[j-1], domain[j], j);
+				right = iterate(domain[j], domain[j+1], j);
 			}
 			fluxes[j].rhodot = left.rhodot-right.rhodot;
 			fluxes[j].momentumdot = left.momentumdot-right.momentumdot;

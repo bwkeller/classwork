3959da9d6550348fe7acfa81dc14fc007468fc24
Ben Keller <malzraa@gmail.com>
2013-12-03 01:30:41 -0500

    grid lines are helpful

--- phys719/HW2/plotoutput.py
+++ phys719/HW2/plotoutput.py
@@ -1,24 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-ncells=400
+ncells=40
 xmax=2
 dx=2.0*xmax/ncells
 
 if __name__ == "__main__":
-	infile = np.genfromtxt("output.dat")
-	plt.subplot(311)
-	plt.plot(np.arange(-xmax,xmax,dx), infile[:,0])
-	plt.xlabel('x')
-	plt.ylabel('v')
-	plt.subplot(312)
-	plt.plot(np.arange(-xmax,xmax,dx), infile[:,1])
-	plt.xlabel('x')
-	plt.ylabel('p')
-	plt.subplot(313)
-	plt.plot(np.arange(-xmax,xmax,dx), infile[:,2])
-	plt.xlabel('x')
-	plt.ylabel(r'$\rho$')
-	fig = plt.gcf()
-	fig.set_size_inches(8.5,11)
-	plt.savefig("output.pdf")
+    infile = np.genfromtxt("output.dat")
+    plt.subplot(311)
+    plt.plot(np.arange(-xmax,xmax,dx), infile[:,0])
+    plt.xlabel('x')
+    plt.ylabel('v')
+    plt.grid()
+    plt.subplot(312)
+    plt.plot(np.arange(-xmax,xmax,dx), infile[:,1])
+    plt.xlabel('x')
+    plt.ylabel('p')
+    plt.grid()
+    plt.subplot(313)
+    plt.plot(np.arange(-xmax,xmax,dx), infile[:,2])
+    plt.xlabel('x')
+    plt.ylabel(r'$\rho$')
+    plt.grid()
+    fig = plt.gcf()
+    fig.set_size_inches(8.5,11)
+    plt.savefig("output.pdf")
